6609187c2aaec7e40a1ff970969f9b853f0056e1
shail-545 <shailjma07@gmail.com>
2025-11-01 20:22:35 +0530

    Update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Shail from './shail'
 
 function App() {
 
   return (
     <>
-      <h1>Hello Shail</h1>
+     <Shail/>
+     <h1>Hello bhai</h1>
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero ullam tenetur minus quaerat consequuntur autem deserunt ipsam nobis ducimus commodi saepe esse debitis excepturi molestias quibusdam aspernatur error, asperiores assumenda!</p>
     </>
   )
 }
---
+++ vite-project/src/shail.jsx
@@ -0,0 +1,9 @@
+function Shail() {
+    return (    
+        <>
+            <h2>This is Shail Component</h2>
+        </>
+    )
+}   
+
+export default Shail
