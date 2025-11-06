a5a6620c9047fea166ee0d170336c152073cb767
shail-545 <shailjma07@gmail.com>
2025-11-06 11:25:00 +0530

    reactRouterDom

---
+++ reactrouterdom/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Link, NavLink } from 'react-router-dom'
+// import Headers from './components/Header/Header.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>React Router Dom</h1>
+    {/* <Headers/> */}
+
+
+   
+    </>
+  )
+}
+
+export default App
